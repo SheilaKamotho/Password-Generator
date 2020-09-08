b856a76eac7dc36f6728f7c0e730efdfeee7cf53
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-08 13:51:26 +0300

    add new user properties

--- users.py
+++ users.py
@@ -5,3 +5,14 @@ class User:
     """
     
     users = []
+
+def __init__(self, user_name, password):
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+        user_name: new account username
+        password: new account password
+        '''
+        self.user_name = user_name
+        self.password = password
