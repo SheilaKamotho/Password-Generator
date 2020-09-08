cac4de43e4c42beb678bdad7236e1e187c4bf9a3
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-08 13:56:31 +0300

    add set up and teardown methods

--- user_test.py
+++ user_test.py
@@ -1,3 +1,21 @@
 import unittest
 from users import User
 
+class TestUsers(unittest.TestCase):
+    '''
+    Test class that defines test cases for the users class behaviours.
+    '''
+    def tearDown(self):
+
+        '''
+        tearDown method to run after each test case.
+        '''
+
+    User.users = []
+    def setUp(self):
+
+        '''
+        Set up method to run before each test cases.
+        '''
+
+        self.new_user = User("Sheila", "SheeKam") 
