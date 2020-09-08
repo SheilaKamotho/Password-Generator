7b7fb52b94f1d8281c56ce0ad2c123e70d2f44fc
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-08 14:00:47 +0300

    define credential properties

---
+++ credentials.py
@@ -0,0 +1,27 @@
+import random
+import string
+import pyperclip
+
+class Credentials:
+
+    """
+    Class that generates new instances of credentials
+    """
+
+    credentials = []
+
+    def __init__(self, account, username, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+        
+        Args:
+            account: New credentials account name.
+            username : New credentials password.
+            password: New credentials password.
+           
+        '''
+
+        self.account = account
+        self.username = username
+        self.password = password
