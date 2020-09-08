4e3ed92ef09dfab7bfbdc17a9d5aa4225aa00b7f
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-08 14:18:55 +0300

    add commented out behaviour

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 from users import User
 from credentials import Credentials
 import string
+
+# Log in username and password 
+# Add existing account details 
+# Create new account credentials - random pasword/inputted password 
+# View various account credentials and the passwords
+# Delete credentials
+# Copy to clipboard
+# Dictate length of password
+
