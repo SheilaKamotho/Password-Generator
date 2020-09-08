accb509b7d82d24bcb9c1a3e441dd0b05be265db
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-08 13:53:59 +0300

    add method to prove user exists

--- users.py
+++ users.py
@@ -19,3 +19,14 @@ class User:
 
     def create_account(self):
         User.users.append(self)
+
+    @classmethod
+    def user_exist(cls, user_name, password):
+
+        '''
+        Method that checks if a user exists and returns a boolean
+        '''
+        for user in cls.users:
+            if user.user_name == user_name and user.password == password:
+                return True
+        return False
